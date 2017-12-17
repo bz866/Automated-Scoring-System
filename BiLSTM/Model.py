766c9b613b54f4bd0fc113f6f50d007f55ae394c
zl1732 <zl1732@nyu.edu>
2017-12-17 14:51:20 -0500

    BiLSTM

---
+++ BiLSTM/Model.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+class LSTM(nn.Module):
+    """Container module with an encoder, a recurrent module, and a decoder."""
+
+    def __init__(self, rnn_type, vocab_size, embedding_dim, hidden_size, num_layers, dropout=0.2, bidirectional = False, pre_emb=None):
+        super(LSTM, self).__init__()
+        self.encoder = nn.Embedding(vocab_size, embedding_dim)
+        self.rnn = getattr(nn, rnn_type)(embedding_dim, hidden_size, num_layers, bias=False, dropout=dropout, bidirectional=bidirectional)
+        self.decoder = nn.Linear(hidden_size, 1)
+        self.decoder_bi = nn.Linear(hidden_size*2, 1)
+        self.bidirectional = bidirectional
+        self.rnn_type = rnn_type
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.init_weights()
+
+    def init_weights(self, pre_emb=None):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.fill_(0)
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, inputs, hidden):
+        emb = self.encoder(inputs)
+        output, hidden = self.rnn(emb, hidden)
+        output = torch.mean(output, 0)
+        output = torch.squeeze(output)
+        if self.bidirectional:
+            decoded = self.decoder_bi(output)
+        else:
+            decoded = self.decoder(output)
+        return decoded, hidden
+    
+    def init_hidden(self, batch_size):
+        weight = next(self.parameters()).data
+        if self.bidirectional == True:
+            return (Variable(weight.new(self.num_layers * 2, batch_size, self.hidden_size).zero_()),
+                    Variable(weight.new(self.num_layers * 2, batch_size, self.hidden_size).zero_()))
+        else:
+            return (Variable(weight.new(self.num_layers, batch_size, self.hidden_size).zero_()),
+                    Variable(weight.new(self.num_layers, batch_size, self.hidden_size).zero_()))            
